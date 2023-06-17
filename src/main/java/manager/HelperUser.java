b78bfbff5cee5287980912a3ccbf6ad562c364fc
Elena-Leonova <lena.postrash@gmail.com>
2023-06-17 13:30:41 +0300

    ILCarro

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,10 +1,27 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class HelperUser extends HelperBase{
+public class HelperUser extends HelperBase {
 
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+    public void clickLogin(){
+        click(By.xpath("//a[@ng-reflect-router-link='login']"));
+
+    }
+
+    public void fillLoginForm(String email, String password){
+        type(By.id("email"),email);
+        type(By.id("password"), password);
+
+    }
+
+    public void clickSubmit(){
+        click(By.xpath("//button[@type = 'submit']"));
+
+    }
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,18 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
     @Test
-    public void loginPositive(){
+    public void positiveLoginTest(){
+        app.getUser().clickLogin();
+        app.getUser().fillLoginForm("lena.postrash@gmail.com", "Mynameislena1!");
+        app.getUser().clickSubmit();
+        app.getUser().pause(3000);
+        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h2[text()='Logged in success']")));
 
     }
 
