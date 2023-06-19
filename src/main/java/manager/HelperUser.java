6bb1b07daabd85339ab5668541d3fcc933eb97c0
Elena-Leonova <lena.postrash@gmail.com>
2023-06-19 21:01:01 +0300

    CW8

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,7 +1,10 @@
 package manager;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelperUser extends HelperBase {
 
@@ -18,10 +21,39 @@ public class HelperUser extends HelperBase {
         type(By.id("password"), password);
 
     }
+    public void fillLoginForm(User user){
+        type(By.id("email"),user.getEmail());
+        type(By.id("password"), user.getPassword());
+
+    }
+
 
     public void clickSubmit(){
         click(By.xpath("//button[@type = 'submit']"));
 
     }
+    public void openLoginForm(){
+        click(By.xpath("//a[@ng-reflect-router-link='login']"));
+    }
+    public void submitForm(){
+        wd.findElement(By.cssSelector("[type='submit']")).submit();
+    }
 
+    public boolean isLoggedSuccess(){
+        WebDriverWait wait = new WebDriverWait(wd, 10);
+        wait.until(ExpectedConditions.visibilityOf(wd.findElement(By.partialLinkText("success"))));
+     return wd.findElement(By.partialLinkText("success")).getText().contains("success");
+    }
+
+    public void clickButton(){
+        click(By.xpath("//button[.='Ok']"));
+    }
+
+    public boolean isLogged(){
+    return isElementPresent(By.xpath("//a[.=' Logout ']"));
+    }
+
+    public void logout(){
+        click(By.xpath("//a[.=' Logout ']"));
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,19 +1,55 @@
 package tests;
 
+import models.User;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
-
+    @BeforeMethod
+    public void precondition(){
+        if(app.getUser().isLogged()) app.getUser().logout();
+    }
     @Test
     public void positiveLoginTest(){
-        app.getUser().clickLogin();
+//        app.getUser().clickLogin();
+//        app.getUser().fillLoginForm("lena.postrash@gmail.com", "Mynameislena1!");
+//        app.getUser().clickSubmit();
+//        app.getUser().pause(3000);
+//        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h2[text()='Logged in success']")));
+        app.getUser().openLoginForm();
         app.getUser().fillLoginForm("lena.postrash@gmail.com", "Mynameislena1!");
-        app.getUser().clickSubmit();
-        app.getUser().pause(3000);
-        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h2[text()='Logged in success']")));
+        app.getUser().submitForm();
+        app.getUser().pause(5000);
+        Assert.assertTrue(app.getUser().isLoggedSuccess());
 
     }
 
+
+
+    @Test
+    public void positiveLoginUser(){
+//        app.getUser().clickLogin();
+//        app.getUser().fillLoginForm("lena.postrash@gmail.com", "Mynameislena1!");
+//        app.getUser().clickSubmit();
+//        app.getUser().pause(3000);
+//        Assert.assertTrue(app.getUser().isElementPresent(By.xpath("//h2[text()='Logged in success']")));
+        User user = new User().withEmail("lena.postrash@gmail.com").withPassword("Mynameislena1!");
+        //user.setName("John");
+        //user.setLastName("Silver");
+
+        app.getUser().openLoginForm();
+//        app.getUser().fillLoginForm("lena.postrash@gmail.com", "Mynameislena1!");
+        app.getUser().fillLoginForm(user);
+        app.getUser().submitForm();
+//        app.getUser().pause(5000);
+//        Assert.assertTrue(app.getUser().isLoggedSuccess());
+
+    }
+    @AfterMethod
+    public void postCondition(){
+        app.getUser().clickButton();
+    }
 }
